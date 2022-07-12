5878af5af588c5c2dd5682686d493403796f6465
abhishek <bhattabhishek636@gmail.com>
2022-07-12 21:50:38 +0530

    hydro-page

---
+++ block-BRaaav/hydro/src/components/Header.js
@@ -0,0 +1,62 @@
+import { AiFillFacebook,AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
+function Header() {
+    return (
+      <>
+        <header className="navbar">
+          <div className="container flex">
+            <div className="flex">
+              <a className="brand" href="index.html">
+                <strong>Hydro</strong>
+              </a>
+              <nav>
+                <ul className="flex nav-menu nav-menu-primary">
+                  <li>
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li>
+                    <a href="index.html">About</a>
+                  </li>
+                  <li>
+                    <a href="index.html">Blog</a>
+                  </li>
+                  <li>
+                    <a href="index.html">Our Work</a>
+                  </li>
+                  <li>
+                    <a href="index.html">Contacts</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <nav>
+              <ul className="flex nav-menu nav-menu-secondary">
+                <li className="social-media-item">
+                  <a href="index.html">
+                  <AiFillFacebook/>
+                  </a>
+                </li>
+                <li className="social-media-item">
+                  <a href="index.html">
+                    <AiOutlineTwitter/>
+                  </a>
+                </li>
+                <li className="social-media-item">
+                  <a href="index.html">
+                    <AiOutlineInstagram/>
+                  </a>
+                </li>
+                <li>
+                  <a className="btn btn-primary" href="index.html">
+                    {" "}
+                    Sign in / Join{" "}
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </>
+    );
+  }
+  
+export default Header;
